84870f5a2e191f5ff3a0e1eb8bf28f5ff57970be
PauLopez05 <167972699+PauLopez05@users.noreply.github.com>
2026-05-08 15:22:37 +0200

    Delete Assets directory

--- Assets/Scripts/Asteroids/asteroid_prefab.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "asteroid_prefab", menuName = "Scriptable Objects/asteroid_prefab")]
-public class asteroid_prefab : ScriptableObject
-{
-    [SerializeField] public GameObject[] asteroids_l;
-}
